fc246f9f452505b0ad5fc6a1a05462b871d85c99
Abobys14 <damalinitskiy@edu.hse.ru>
2023-07-07 15:15:53 +0300

    ver. 1.0

---
+++ functions.py
@@ -0,0 +1,7 @@
+def get_text_messages(message, bot):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import telebot
+from functions import get_text_messages
+
 with open('token.txt', 'r') as file:
     token = file.read().strip()
 
 
-
-import telebot;
 bot = telebot.TeleBot(token);
+@bot.message_handler(content_types=['text'])
+def handle_text_messages(message):
+    get_text_messages(message, bot)
+
+bot.polling(none_stop=True, interval=0)
