6e3b16cdc33869cbbfdd928061342bca050eb2b4
Abobys14 <damalinitskiy@edu.hse.ru>
2023-07-07 15:53:02 +0300

    ver 1.1.1

--- message.py
+++ message.py
@@ -1,7 +1,5 @@
 def get_text_messages(message, bot):
-    if message.text == "Привет":
+    if message.text == "/start":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Привет")
     else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /start.")
