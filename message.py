105ad27a2c023dd79946a6009bdae50cbf99c544
Abobys14 <damalinitskiy@edu.hse.ru>
2023-07-09 15:04:57 +0300

    ver 1.4 stable

--- message.py
+++ message.py
@@ -7,26 +7,16 @@ def get_text_messages(message, bot):
         bot.send_message(message.from_user.id, "Привет, напиши /vpn")
         
     elif message.text == "/vpn":
-        bot.send_message(message.from_user.id, "Нужен VPN? Подожди 10 секунд")
-        name = message.from_user.first_name
-
-        
-
-        def contains_cyrillic(name):
-            return bool(re.search('[а-яА-Я]', name))
-        if contains_cyrillic(name)==True:
-            now=datetime.now()
-            name=now
-        if len(name) > 15:
-             name = name[:15]
+        bot.send_message(message.from_user.id, "Почти готово, подожди 15 секунд")
+        name = str(message.from_user.id)
         get_profil(name)
-        print('cool')
+        print(name)
 
 
-        document = open('/Users/aboba/Desktop/vpn_bot2/ecom/wg0-client-'+name+'.conf', 'rb')
+        document = open('/Users/aboba/Desktop/vpn_bot2/ecom/'+name+'.conf', 'rb')
         bot.send_message(message.from_user.id, "Готово:")
         bot.send_document(message.chat.id, document)
-
+    
     else:
         bot.send_message(message.from_user.id, "Напиши /start.")
         
